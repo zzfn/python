df2a95958efd009a77d8bb609e09b78db002b6fb
chenc <chenc@fengyuntec.com>
2021-01-13 17:27:49 +0800

    f i

--- zhangzifeng.py
+++ zhangzifeng.py
@@ -12,15 +12,23 @@ id = '1353112775'
 na = 'a'
 # 设置代理IP
 
-iplist = ['60.13.42.8:9999', '117.90.0.172:9000', '121.232.148.156:9000', '120.234.138.99:53779', '121.232.148.15:9000',
-          '183.161.29.127:8060', '106.12.42.237:3128', '106.110.195.3:9999', '120.79.64.147:8118']
+iplist = ['113.195.147.93:9999', '123.169.127.142:9999', '123.169.117.120:9999']
 
-proxy_addr = "60.11.5.104:80"
+proxy_addr = "113.117.66.85:9999"
 
 
 # 定义页面打开函数
 def use_proxy(url, proxy_addr):
-    req = urllib.request.Request(url, )
+    req = urllib.request.Request(url)
+    req.add_header("User-Agent",
+                   "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.221 Safari/537.36 SE 2.X MetaSr 1.0")
+    req.add_header("cookie",
+                   "ALF=1563501151; SCF=Apx1PRwrfB4If6VYDuCoe_oorlZ5gXCWgirtv_8vjRInbSiwiRjXJzR1HlwIwMRHub8Qq-MsnmzwbCu94WX7vlA.; SUB=_2A25wDe0fDeRhGeVH71AT9yrPzTmIHXVT8fNXrDV6PUJbktANLWrRkW1NT0BnhRvEpps1o8r4U5Lq6WlEI11t3hu5; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WhgALk5OZhvyM8uOfTuxHgy5JpX5K-hUgL.Foe4ShzES0B0So-2dJLoIpjLxKnLB.BLB-qLxK-LBK-LBoqLxKML1h.L1-zt; SUHB=0idrAQnnVzMAp9; MLOGIN=1; _T_WM=62061002588; WEIBOCN_FROM=1110006030; XSRF-TOKEN=afb963; M_WEIBOCN_PARAMS=luicode%3D10000011%26lfid%3D1005051353112775%26oid%3D4244627785851269%26fid%3D1078031353112775%26uicode%3D10000011")
+    proxy = urllib.request.ProxyHandler({'http': random.choice(iplist)})
+    opener = urllib.request.build_opener(proxy, urllib.request.HTTPHandler)
+    urllib.request.install_opener(opener)
+    data = urllib.request.urlopen(req).read().decode('utf-8', 'ignore')
+    return data
 
 
 # 获取微博主页的containerid，爬取微博内容时需要此id
@@ -55,8 +63,8 @@ def get_userInfo(id):
 
 # 获取微博内容信息,并保存到文本中，内容包括：每条微博的内容、微博详情页面地址、点赞数、评论数、转发数等
 def get_weibo(id, file):
-    i = 22
-    Directory = "C:\zhangzifeng\MM\/"
+    i = 1
+    Directory = "/Users/zzf/Desktop/dev/python/zzf/"
     while True:
         url = 'https://m.weibo.cn/api/container/getIndex?type=uid&value=' + id
         weibo_url = 'https://m.weibo.cn/api/container/getIndex?type=uid&value=' + id + '&containerid=' + get_containerid(
@@ -107,6 +115,6 @@ def get_weibo(id, file):
 
 
 if __name__ == "__main__":
-    file = 'C:\zhangzifeng\\' + id + ".txt"
+    file = '/Users/zzf/Desktop/dev/python\\' + id + ".txt"
     get_userInfo(id)
     get_weibo(id, file)
